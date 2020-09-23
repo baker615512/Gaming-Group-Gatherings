833ba26b45f524270f31605ab4b725528cd1cef2
baker615512 <baker615512@gmail.com>
2020-09-23 18:29:42 -0500

    WIP: added signup and login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/signup', to: 'gamers#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   resources :gatherings
   resources :groups
   resources :gamers
