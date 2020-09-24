c029ac7ce5b493f5d9de00bb61e0a83a50c76ddc
baker615512 <baker615512@gmail.com>
2020-09-23 19:08:07 -0500

    WIP: created new gamer methods

--- app/controllers/gamers_controller.rb
+++ app/controllers/gamers_controller.rb
@@ -5,6 +5,22 @@ class GamersController < ApplicationController
     end
 
     def create
+        @gamer = Gamer.new(gamer_params)
+        if @gamer.save
+            session[:gamer_id] = @gamer.id
+            redirect_to gamer_path(@gamer)
+        else
+            render :new
+        end
+    end
+    
+    def show
+        @gamer = Gamer.find_by(params[:gamer_id])
+    end
 
+    private
+
+    def gamer_params
+        params.require(:gamer).permit(:username, :password)
     end
 end
