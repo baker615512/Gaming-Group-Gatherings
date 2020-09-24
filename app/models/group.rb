969688df32a99adc8f4ec7c0bf89229a9e66a0f1
baker615512 <baker615512@gmail.com>
2020-09-24 17:35:02 -0500

    WIP: added validations to group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,6 @@
 class Group < ApplicationRecord
     has_many :gatherings
     has_many :gamers, through: :gatherings
+    validates :game_title, uniqueness: true, presence: true
+    validates :game_category, presence: true
 end
