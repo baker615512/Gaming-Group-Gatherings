c073c742f0eb3f7795aa729c6c5a8de63e470e82
baker615512 <baker615512@gmail.com>
2020-09-23 22:16:17 -0500

    WIP: created login form and methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,12 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @gamer = Gamer.find_by(username: params[:gamer][:username])
+    if @gamer && @gamer.authenticate(params[:gamer][:password])
+      session[:gamer_id] = @gamer.id
+      redirect_to '/gamer/:id'
+    else
+      render :new
   end
 
   def destroy
