d56cf9168b3f4a4288dd16c72d2026ba7ef0361a
baker615512 <baker615512@gmail.com>
2020-09-23 19:09:29 -0500

    WIP: added validation for gamer username

--- app/models/gamer.rb
+++ app/models/gamer.rb
@@ -2,4 +2,5 @@ class Gamer < ApplicationRecord
     has_secure_password
     has_many :gatherings
     has_many :groups, through: :gatherings
+    validates :username, uniqueness: true, presence: true
 end
