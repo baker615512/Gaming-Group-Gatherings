2d4fa50e2992774eb0f79de3649ebaef6296bfcc
baker615512 <baker615512@gmail.com>
2020-09-24 20:45:58 -0500

    WIP: added create method for groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,6 +5,17 @@ class GroupsController < ApplicationController
     end
 
     def create
+        @group = Group.new(group_params)
+        if @group.save
+            redirect_to group_path(@group)
+        else
+            render :new
+        end
+    end
 
+    private
+
+    def group_params
+        params.require(:group).permit(:game_title, :game_category)
     end
 end
