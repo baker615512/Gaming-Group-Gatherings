2adc34d99c8122475aacda14d370796e7750027c
baker615512 <baker615512@gmail.com>
2020-09-25 08:24:43 -0500

    WIP: added validations to gathering model

--- app/models/gathering.rb
+++ app/models/gathering.rb
@@ -1,4 +1,7 @@
 class Gathering < ApplicationRecord
   belongs_to :gamer
   belongs_to :group
+  validates :game_cafe, presence: true
+  validates :day_of_week, presence: true
+  validates :group_id, presence: true
 end
