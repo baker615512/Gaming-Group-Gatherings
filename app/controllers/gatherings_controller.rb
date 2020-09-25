51dee43345473a30ab49c7700dab39f7e88659f2
baker615512 <baker615512@gmail.com>
2020-09-25 16:42:46 -0500

    WIP: added create method to gatherings controller

--- app/controllers/gatherings_controller.rb
+++ app/controllers/gatherings_controller.rb
@@ -10,13 +10,14 @@ class GatheringsController < ApplicationController
     end
 
     def create
-
+        @gathering = current_user.gatherings.create(gathering_params)
+        redirect_to gathering_path(@gathering)
     end
 
     private
 
     def gathering_params
         params.require(:gathering).permit(:game_cafe, :day_of_week,
-        :group_id, group_attributes: %i[game_title game_category])
+        :group_id, group_attributes: [:game_title, :game_category])
     end
 end
