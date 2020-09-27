94c2f15e8c9e39fbb413ef016125c03c4d6206c2
baker615512 <baker615512@gmail.com>
2020-09-26 20:24:39 -0500

    WIP: debugged current user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
     private
 
     def current_user
-        @current_user ||= User.find_by_id(session[:id])
+        @current_user ||= Gamer.find_by_id(session[:gamer_id])
     end
 end
