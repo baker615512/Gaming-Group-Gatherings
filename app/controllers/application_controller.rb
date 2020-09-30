ca5cceea660c9067d20faa48ad8e606c8bb1d60f
baker615512 <baker615512@gmail.com>
2020-09-30 14:22:10 -0500

    WIP: added logged in and redirect helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,14 @@ class ApplicationController < ActionController::Base
     def current_user
         @current_user ||= Gamer.find_by_id(session[:gamer_id])
     end
+
+    def logged_in?
+        !!current_user
+    end
+  
+    def redirect_if_not_logged_in
+        if !logged_in?
+            redirect '/'
+        end
+    end
 end
