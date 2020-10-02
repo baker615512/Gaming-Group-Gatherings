87f78184242bec9fa900c3b7c34c638cd2946a38
baker615512 <baker615512@gmail.com>
2020-10-02 16:23:59 -0500

    WIP: added scope methods for group game categories

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,8 @@ class Group < ApplicationRecord
     has_many :gamers, through: :gatherings
     validates :game_title, uniqueness: true, presence: true
     validates :game_category, presence: true
+
+    scope :board_game, -> { where(game_category: "Board Game") }
+    scope :card_game, -> { where(game_category: "Card Game") }
+    scope :rpg, -> { where(game_category: "RPG") }
 end
