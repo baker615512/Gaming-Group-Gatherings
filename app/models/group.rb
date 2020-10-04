b38fffad77cd1ec94c57772ddd5c087449fb32ea
baker615512 <baker615512@gmail.com>
2020-10-03 22:31:41 -0500

    WIP: removed unnecessary code

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,16 +3,4 @@ class Group < ApplicationRecord
     has_many :gamers, through: :gatherings
     validates :game_title, uniqueness: true, presence: true
     validates :game_category, presence: true
-
-    def self.board_game
-        where(game_category: 'Board Game')
-    end
-
-    def self.card_game
-        where(game_category: 'Card Game')
-    end
-
-    def self.rpg
-        where(game_category: 'RPG')
-    end
 end
