d2364c6ffadb7bfa731fa814138715a6ba212d93
baker615512 <baker615512@gmail.com>
2020-10-04 10:01:05 -0500

    WIP: added class methods to groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,16 @@ class Group < ApplicationRecord
     has_many :gamers, through: :gatherings
     validates :game_title, uniqueness: true, presence: true
     validates :game_category, presence: true
+
+    def self.board_games
+        where(game_category: 'Board Game')
+    end
+
+    def self.card_games
+        where(game_category: 'Card Game')
+    end
+
+    def self.rpg
+        where(game_category: 'RPG')
+    end
 end
